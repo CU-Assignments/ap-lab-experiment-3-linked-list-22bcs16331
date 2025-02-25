060cf47b9e0f5d2cecd08a26146b9c21bfe81e16
22bcs16331 <149153341+22bcs16331@users.noreply.github.com>
2025-02-25 09:57:47 +0530

    Create reverse a linked list.cpp

---
+++ reverse a linked list.cpp
@@ -0,0 +1,14 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode curr = head;
+
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = temp;
+        }
+        return pre;
+    }
+}
